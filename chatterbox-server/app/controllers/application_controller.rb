517fd5f034f6da3560b904418a45b40fe1237d7a
Joshua <j.blengino@yahoo.com>
2021-08-22 20:10:37 -0700

    lab complete

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -3,4 +3,31 @@ class ApplicationController < Sinatra::Base
   
   # Add routes
   
+  get '/messages' do
+    messages = Message.all
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    edited_message = Message.find(params[:id])
+    edited_message.update(
+      body: params[:body],
+      username: params[:username]
+    )
+    edited_message.to_json
+  end
+
+  delete '/messages/:id' do
+    deleted_message = Message.find(params[:id])
+    deleted_message.destroy
+    deleted_message.to_json
+  end
 end
